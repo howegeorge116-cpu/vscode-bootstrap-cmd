3b1356ada93152266c15eba7a64afff884e39a0d
slyvain <harbingervaughnxnkwr95486@outlook.com>
2025-12-26 10:58:10 +0900

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = 5000;
+
+express()
+    .use('/settings', require('./api/routes'))
+    .listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
