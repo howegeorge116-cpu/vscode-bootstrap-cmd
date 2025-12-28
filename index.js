c2f99a4283707fe60755bddbb347bbaa7727a2e5
slyvain <harbingervaughnxnkwr95486@outlook.com>
2025-12-29 01:09:04 +0900

    fix

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 const PORT = 5000
 
 const app = express()
-    // .use('/', (req, res) => res.send('VSCode Setup'))
-    .use('/', express.static('public'))
-    .use('/settings', require('./api/routes'))
+    .use('/', (req, res) => res.send('VSCode Setup'))
+    .use('/settings', require('./api'))
 
 export default app;
